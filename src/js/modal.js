9c171c0e50f4a7bb3119eb02eff91767e4d0b90c
LordCracken <vladislav.yakimovskiy@mail.ru>
2021-08-26 17:17:45 +0300

    added closing modal by clicking backdrop

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,24 +1,30 @@
 $(document).ready(function () {
-  var body = $('body');
-  var navPhoneBtn = $('#nav-phone-btn');
-  var priceLink = $('.card__price');
-  var closeBtn = $('#close-btn');
-  var modal = $('#default-modal');
+  var body = $("body");
+  var navPhoneBtn = $("#nav-phone-btn");
+  var priceLink = $(".card__price");
+  var closeBtn = $("#close-btn");
+  var modal = $("#default-modal");
 
   // Скрипт для вызова модального окна кнопкой в шапке
-  navPhoneBtn.on('click', function () {
-    modal.addClass('modal_active');
-    body.addClass('opened-modal');
+  navPhoneBtn.on("click", function () {
+    modal.addClass("modal_active");
+    body.addClass("opened-modal");
   });
 
-  priceLink.on('click', function () {
-    modal.addClass('modal_active');
-    body.addClass('opened-modal');
+  priceLink.on("click", function () {
+    modal.addClass("modal_active");
+    body.addClass("opened-modal");
   });
 
-  closeBtn.on('click', function () {
-    modal.removeClass('modal_active');
-    body.removeClass('opened-modal');
+  closeBtn.on("click", function () {
+    modal.removeClass("modal_active");
+    body.removeClass("opened-modal");
   });
 
-}); 
+  modal.on("click", function (e) {
+    if (e.target.id === "default-modal") {
+      modal.removeClass("modal_active");
+      body.removeClass("opened-modal");
+    }
+  });
+});
